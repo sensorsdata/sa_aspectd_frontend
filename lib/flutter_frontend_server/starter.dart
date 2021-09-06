bf04dbfdcd22f27055f18c6ae9a038f85a1afa6d
张伟 <zhangwei@sensorsdata.cn>
2021-09-06 16:08:28 +0800

    多入口支持

--- lib/flutter_frontend_server/starter.dart
+++ lib/flutter_frontend_server/starter.dart
@@ -10,6 +10,26 @@ import 'dart:io';
 import 'server.dart';
 
 Future<void> main(List<String> args) async {
+  // List<String> newArgs = [];
+  // newArgs.add("--sdk-root");
+  // newArgs.add("/Users/zhangwei/Documents/tools/flutter/bin/cache/artifacts/engine/common/flutter_patched_sdk/");
+  // newArgs.add("--target=flutter");
+  // newArgs.add("--no-print-incremental-dependencies");
+  // newArgs.add("-Dflutter.inspector.structuredErrors=true");
+  // newArgs.add("-DFLUTTER_WEB_AUTO_DETECT=true");
+  // newArgs.add("-Ddart.vm.profile=false");
+  // newArgs.add("-Ddart.vm.product=false");
+  // newArgs.add("--enable-asserts");
+  // newArgs.add("--track-widget-creation");
+  // newArgs.add("--no-link-platform");
+  // newArgs.add("--packages");
+  // newArgs.add("/Users/zhangwei/Documents/work/flutter_workspace/ssss/testdemo/.dart_tool/package_config.json");
+  // newArgs.add("--output-dill");
+  // newArgs.add("/Users/zhangwei/Documents/work/flutter_workspace/ssss/testdemo/.dart_tool/flutter_build/abe58c1d1006e8bf25cdc6f8ce0a8dc2/app.dill");
+  // newArgs.add("--depfile");
+  // newArgs.add("/Users/zhangwei/Documents/work/flutter_workspace/ssss/testdemo/.dart_tool/flutter_build/abe58c1d1006e8bf25cdc6f8ce0a8dc2/kernel_snapshot.d");
+  // newArgs.add("package:testdemo/main.dart");
+
   final int exitCode = await starter(args);
   if (exitCode != 0) {
     exit(exitCode);
